74f6c1e7a23ad786d7f3108a615b00be3ce124ab
seffers122 <eseth51@yahoo.com>
2015-04-10 18:09:33 -0600

    Customization of tails to fit what i'm doing

--- src/main/java/kihira/tails/client/gui/dialog/IDialogCallback.java
+++
@@ -1,8 +0,0 @@
-package kihira.tails.client.gui.dialog;
-
-import net.minecraft.client.gui.GuiButton;
-
-public interface IDialogCallback {
-
-    public void buttonPressed(Dialog dialog, GuiButton button);
-}
